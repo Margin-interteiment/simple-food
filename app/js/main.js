a6eae3c1360cc04cf96abc1badd6ba919cfdca47
Margin-enterteiment <119576487+Margin-interteiment@users.noreply.github.com>
2024-03-02 20:46:34 +0200

    added all pages and fix they

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function () {
+  $(".discount-inner__list--active").slick({
+    dots: true,
+    arrows: false,
+  });
+
+  $(".header__menu-button").on("click", function () {
+    $(".header__list").toggleClass("menu__list--active");
+    $(".header__menu-button").toggleClass(".header__menu-button--active");
+    $("body").toggleClass("lock");
+  });
+
+  $(".header__menu-button--active").on("click", function () {
+    $(".header__list").removeClass("menu__list--active");
+  });
+
   $(".filter-price__input").ionRangeSlider({
     // skin: "modern",
     type: "double",
@@ -20,6 +35,11 @@ $(function () {
       '<button type="button" class="reviews__arrows   reviews--prev"> <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
     nextArrow:
       '<button type="button" class="reviews__arrows reviews--next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
+  });
+
+  $(".restaurant__item--active").slick({
+    dots: true,
+    arrows: false,
   });
 
   $(".burger-one__slide").slick({
@@ -84,9 +104,6 @@ modalClose.on("click", function (event) {
   $("body").removeClass("no-scroll");
 });
 
-
-
-
 // $(".modal-window__content").slick({
 //   // infinite: true,
 //   arrows: true,
@@ -94,9 +111,6 @@ modalClose.on("click", function (event) {
 //   slidesToShow: 3,
 //   slidesToScroll: 3,
 // });
-
-
-
 
 // let timer = null;
 // let slider = document.querySelector("#slider"),
@@ -124,8 +138,6 @@ modalClose.on("click", function (event) {
 //   document.getElementById("wrap9").style.display = state;
 // }
 
-
-
 $(".product-tabs__top-item").on("click", function (e) {
   e.preventDefault();
 
@@ -140,44 +152,48 @@ $(".product-tabs__top-item").on("click", function (e) {
   $($(this).attr("href")).addClass("product-tabs__content-item--active");
 });
 
+$(".interests-slider__menu").slick({
+  arrows: true,
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+        dots: true,
+      },
+    },
 
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
 
- $(".interests-slider__menu").slick({
-   arrows: true,
-   dots: false,
-   infinite: true,
-   slidesToShow: 4,
-   slidesToScroll: 5,
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
 
-   prevArrow:
-     '<button type="button" class="slick-arrow interests-slider__prev"> <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
-   nextArrow:
-     '<button type="button" class="slick-arrow interests-slider__next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
- });
+  prevArrow:
+    '<button type="button" class="slick-arrow interests-slider__prev"> <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
+  nextArrow:
+    '<button type="button" class="slick-arrow interests-slider__next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg></button>',
+});
 
-
-
- $(".modal-window__content").slick({
-   arrows: true,
-   dots: true,
-   prevArrow:
-     '<button type="button" class="modal__prev"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M21.0383 31.0149L37.0516 45.6079C37.5771 46.1307 38.4293 46.1307 38.9548 45.6079C39.4802 45.0851 39.4802 44.2369 38.9548 43.7141L23.907 30L38.9534 16.2859C39.4789 15.7631 39.4789 14.9148 38.9534 14.3921C38.428 13.8693 37.5758 13.8693 37.0503 14.3921L21.037 28.9851C20.7569 29.2638 20.6369 29.6332 20.6556 29.9986C20.6381 30.3654 20.7582 30.7348 21.0383 31.0149Z" fill="#505050"/></svg> </button>',
-   nextArrow:
-     '<button type="button" class="modal__next"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M38.9641 28.9851L22.9508 14.3921C22.4254 13.8693 21.5731 13.8693 21.0477 14.3921C20.5222 14.9149 20.5222 15.7631 21.0477 16.2859L36.0954 30L21.049 43.7141C20.5235 44.2369 20.5235 45.0852 21.049 45.6079C21.5745 46.1307 22.4267 46.1307 22.9521 45.6079L38.9654 31.0149C39.2455 30.7362 39.3656 30.3668 39.3469 30.0014C39.3643 29.6346 39.2443 29.2652 38.9641 28.9851Z" fill="#505050"/></svg></button>',
- });
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
+$(".modal-window__content").slick({
+  arrows: true,
+  dots: true,
+  prevArrow:
+    '<button type="button" class="modal__prev"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M21.0383 31.0149L37.0516 45.6079C37.5771 46.1307 38.4293 46.1307 38.9548 45.6079C39.4802 45.0851 39.4802 44.2369 38.9548 43.7141L23.907 30L38.9534 16.2859C39.4789 15.7631 39.4789 14.9148 38.9534 14.3921C38.428 13.8693 37.5758 13.8693 37.0503 14.3921L21.037 28.9851C20.7569 29.2638 20.6369 29.6332 20.6556 29.9986C20.6381 30.3654 20.7582 30.7348 21.0383 31.0149Z" fill="#505050"/></svg> </button>',
+  nextArrow:
+    '<button type="button" class="modal__next"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M38.9641 28.9851L22.9508 14.3921C22.4254 13.8693 21.5731 13.8693 21.0477 14.3921C20.5222 14.9149 20.5222 15.7631 21.0477 16.2859L36.0954 30L21.049 43.7141C20.5235 44.2369 20.5235 45.0852 21.049 45.6079C21.5745 46.1307 22.4267 46.1307 22.9521 45.6079L38.9654 31.0149C39.2455 30.7362 39.3656 30.3668 39.3469 30.0014C39.3643 29.6346 39.2443 29.2652 38.9641 28.9851Z" fill="#505050"/></svg></button>',
+});
